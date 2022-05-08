2029e527abb18902c9e6ad5aaab4a702cc79d3e3
Ibnballo1 <belloabdullateef035@gmail.com>
2022-05-08 12:17:10 +0100

    Updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,23 @@
-let slideShow = document.querySelectorAll('.slide');
-console.log(slideShow);
+// First select the slide's wrapper
+// which the select element
+
+let slideShows = document.querySelectorAll('[data-slides = "slideBody"]');
+slideShows.forEach(beginSlideShow);
+
+function beginSlideShow(slideshow) {
+    let slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`);
+
+    let index = 0, time = 5000;
+    slides[index].classList.add('active');
+
+    setInterval(()=> {
+        slides[index].classList.remove('active');
+    
+        index++;
+        if (index === slides.length) {
+            index = 0;
+        }
+        slides[index].classList.add('active');
+    }, time);
+    //console.log(slides);
+}
